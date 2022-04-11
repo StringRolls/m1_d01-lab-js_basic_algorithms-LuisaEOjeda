e0fd542fdab6b1908b79a5d515ae31326d1a2499
Luisa Ojeda <luisaojeda@gmail.com>
2022-04-11 22:39:43 +0200

    missing palindromes

--- js/index.js
+++ js/index.js
@@ -62,7 +62,32 @@ for (let i = 0; i < myArray.length; i++){
     }
 }
 
-
 // Bonus 1: Palindromes
 
+
+
 // Bonus 2: Lorem ipsum counter
+
+let myStringLorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis unde laboriosam accusamus magnam et doloribus animi ducimus, dicta excepturi perferendis corporis porro repellendus quidem sed autem recusandae voluptatem officiis! Facere!Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+let wordCount = myStringLorem.split(" ").length
+log (wordCount);
+
+//log(typeof wordCount)
+
+// count the number of times the Latin word et appears.
+
+function countString(str, word) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str.charAt(i) == word) {
+            count = count + 1;
+        }
+    }
+    return count;
+};
+
+log(countString(myStringLorem, 'et'));
+
